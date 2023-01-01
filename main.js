0085d4422cedf0770fe97b28af86857b9257d47e
SeeQuentt <120139996+SeeQuentt@users.noreply.github.com>
2023-01-01 22:41:51 +0100

    Work in progress

---
+++ main.js
@@ -0,0 +1,8 @@
+const resultsLength = document.getElementById("results-length")
+const resultsVolume = document.getElementById("results-volume")
+const resultsMass = document.getElementById("results-mass")
+
+const amountIn = document.getElementById("amount").value
+
+
+
